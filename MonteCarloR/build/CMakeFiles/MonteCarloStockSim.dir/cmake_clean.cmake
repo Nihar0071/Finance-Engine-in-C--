ba6a056775db018a12eeb9d3f0fb4427d091ba3a
nihar-patel <niharpatel007rd@gmail.com>
2024-09-25 13:13:43 -0400

    Imported the Data in CPP file

---
+++ MonteCarloR/build/CMakeFiles/MonteCarloStockSim.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MonteCarloStockSim.dir/main.cpp.o"
+  "CMakeFiles/MonteCarloStockSim.dir/main.cpp.o.d"
+  "MonteCarloStockSim"
+  "MonteCarloStockSim.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MonteCarloStockSim.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
